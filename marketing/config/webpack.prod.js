e4b0e4f89ed56cc4701874f0e7656288c1a6e903
TienDat1999 <letiendat1999py@gmail.com>
2023-06-21 07:23:19 +0700

    update public path

--- marketing/config/webpack.prod.js
+++ marketing/config/webpack.prod.js
@@ -7,7 +7,7 @@ const prodConfig = {
     mode: 'development',
     output: {
         filename: '[name].[contenthash].js',
-        publicPath: '/marketing/latest'
+        publicPath: '/marketing/latest/'
     },
     plugins: [
         new ModuleFederationPlugin({
