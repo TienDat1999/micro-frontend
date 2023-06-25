d8347fd064a5d931b644709cf4556c7a5015d63d
TienDat <letiendat1999py@gmail.com>
2023-06-25 09:03:08 +0700

    update err router

--- container/src/App.js
+++ container/src/App.js
@@ -1,12 +1,16 @@
 import React from "react";
 import MarketingApp from "./components/MarketingApp";
 import Header from './components/Header'
+import { BrowserRouter, Route } from 'react-router-dom'
 
 export default () => {
   return (
-    <div>
-     <Header/>
-      <MarketingApp />
-    </div>
+    <BrowserRouter>
+     {/* <Route path="/" component={Main} /> */}
+      <div>
+      <Header/>
+        <MarketingApp />
+      </div>
+    </BrowserRouter>
   );
 };
